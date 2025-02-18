ffd077f6ef16bf24ee08eaa281c7cdc22b3482c3
ange <secretarioanglique@gmail.com>
2025-02-18 13:29:36 +0800

    2nd commit on 2nd branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
+interface Post {
+  id: number;
+  title: any;
+  content: any;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'secretario';
+  title = 'it_elec-6a';
+  storedPosts:  Post[] = [];
+  onPostAdded(post: any): void{
+    this.storedPosts.push(post);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,28 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post.create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
   ],
   
   imports: [
@@ -25,8 +36,12 @@ import {MatInputModule} from '@angular/material/input';
     FormsModule,
     MatFormFieldModule, 
     MatInputModule,
-
-       
+    MatToolbarModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,    
   ],
   providers: [],
   bootstrap: [AppComponent]
